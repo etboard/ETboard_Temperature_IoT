2744e63b9610e6768f0ac1c58f79f5957cb97c03
ETboard <106308185+etboard@users.noreply.github.com>
2023-10-27 21:01:03 +0900

    Update run.bat

--- src/python/logger/run.bat
+++ src/python/logger/run.bat
@@ -1,11 +1,22 @@
 @echo off
-@chcp 65001
+@cls
+@chcp 65001 > nul
+
+@echo ##################################################################################
+@echo # ETboard IoT Logger V0.91
+@echo # ================================================================================
+@echo # HTTP://ET.KETRi.RE.KR   Copyright 2022. KETRi. All rights reserved.
+@echo ##################################################################################
+
 if [%1]==[] goto :blank
-@echo "이티보드 맥어드레스는 " %1
+
+@echo "Mac address of ETboard " %1
 dist\mqtt-data-logger -b broker.hivemq.com -p 1883 -t %1/et/smpl/tele/sensor -n data-logger
 @pause
 
 :blank
-@echo "이티보드 맥어드레스를 입력하세요"
-@echo "예) run 96:EA:E8"
+@echo:
+@echo Please run with the mac address of ETboard like as below
+@echo run 96:EA:E8
+@echo:
 @pause
