84ec6247cbfcd60febea3f4217ae6a8d9ee916d7
ETboard <106308185+etboard@users.noreply.github.com>
2023-10-27 18:04:51 +0900

    Update run.bat

--- src/python/logger/run.bat
+++ src/python/logger/run.bat
@@ -4,7 +4,6 @@ if [%1]==[] goto :blank
 @echo "이티보드 맥어드레스는 " %1
 dist\mqtt-data-logger -b broker.hivemq.com -p 1883 -t %1/et/smpl/tele/sensor -n data-logger
 @pause
-@exit
 
 :blank
 @echo "이티보드 맥어드레스를 입력하세요"
